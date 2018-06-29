769e5cfac15f81991f3be8686cdb22ea24aef8f9
demotive <demotive@gmail.com>
2018-06-29 13:38:18 +0100

    Kill everything to do with the Register with a GP prototype

--- server.js
+++ server.js
@@ -59,16 +59,8 @@ app.use(session({
 
 // Add variables that are available in all views
 app.use(function (req, res, next) {
-  res.locals.serviceName = config.serviceName
-  res.locals.practiceName = config.practiceName
-  res.locals.practiceAddress = config.practiceAddress
-  res.locals.practicePostcode = config.practicePostcode
-  res.locals.practiceTelephone = config.practiceTelephone
-  res.locals.practiceEmail = config.practiceEmail
   res.locals.cookieText = config.cookieText
   res.locals.session = req.session
-  res.locals.gpLookupURL = gpLookupURL
-  res.locals.mapsKey = mapsKey;
   res.locals.jsNow = new Date();
   next()
 })
