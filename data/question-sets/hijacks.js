0df5e2de59478e372225ee6c8db51f3526990285
demotive <demotive@gmail.com>
2018-07-06 10:12:35 +0100

    Add some headache journey questions

--- data/question-sets/hijacks.js
+++ data/question-sets/hijacks.js
@@ -6,6 +6,22 @@ var hijacks = [
   {
     "pwid": "PW503",
     "slug": "stomach-or-side-injury-without-a-cut-or-wound"
+  },
+  {
+    "pwid": "PW752",
+    "slug": "headache-or-migraine"
+  },
+  {
+    "pwid": "PW753",
+    "slug": "headache-or-migraine"
+  },
+  {
+    "pwid": "PW755",
+    "slug": "headache-or-migraine"
+  },
+  {
+    "pwid": "PW756",
+    "slug": "headache-or-migraine"
   }
 ];
 
