3944893c02e257deb5b7a8baee70be9f2ad3f9be
demotive <demotive@gmail.com>
2018-10-04 11:59:35 +0100

    Split demographics

--- app/routes/start.js
+++ app/routes/start.js
@@ -8,64 +8,45 @@ module.exports = router
 // Journey to questions - August 2018 ++++++++++++++++++++++++++++++++++++++++++
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-router.post('/demographics', function(req, res) {
-  // "Adult" is 16+
-  // Set a default here if there's a lack of req.query
-  // Male
-  // Adult (40)
+router.post('/date-of-birth', function(req, res) {
   if (!req.session.demographics) {
-    // zero out a namespaced session obj with defaults
     req.session.demographics = {};
-    req.session.demographics.sex = 'Male';
-    req.session.demographics.age = '40';
-    req.session.demographics.ageCategory = 'Adult';
     req.session.demographics.dob = {};
-    req.session.demographics.dob.day = '23';
-    req.session.demographics.dob.month = '5';
-    req.session.demographics.dob.year = '1977';
-    req.session.demographics.dob.supplied = false;
   }
   if (req.body['dob-day'] !== '' && req.body['dob-month'] !== '' && req.body['dob-year'] !== '') {
     var year = req.body['dob-year']
     var month = req.body['dob-month']
     var day = req.body['dob-day']
-
     var dob = moment().set({
       'year': year,
       'month': month,
       'date': day
     });
-
     var age = moment().diff(dob, 'years')
-
     var ageCategory = 'Adult';
     if (age < 16) {
       ageCategory = 'Child';
     }
-
     req.session.demographics.dob.year = year;
     req.session.demographics.dob.month = month;
     req.session.demographics.dob.day = day;
     req.session.demographics.dob.supplied = true;
     req.session.demographics.age = age;
     req.session.demographics.ageCategory = ageCategory;
-
   } else if (req.body['age'] !== '') {
-
     var age = Number(req.body['age']);
-
     var ageCategory = 'Adult';
     if (age < 16) {
       ageCategory = 'Child';
     }
-
-    req.session.demographics.dob.year = '';
-    req.session.demographics.dob.month = '';
-    req.session.demographics.dob.day = '';
     req.session.demographics.dob.supplied = false;
     req.session.demographics.age = age;
     req.session.demographics.ageCategory = ageCategory;
   }
+  res.redirect('/start/sex');
+});
+
+router.post('/sex', function(req, res) {
   if (req.body['sex']) {
     req.session.demographics.sex = req.body['sex'];
   }
