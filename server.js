4a196ae1ddd718cf039c0698788c1eed72d3d833
demotive <demotive@gmail.com>
2018-11-21 21:59:22 +0000

    Basic change location journey

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ var questions = require('./app/routes/questions');
 var forcedCallback = require('./app/routes/forced-callback');
 var callbackOffered = require('./app/routes/callback-offered');
 var bookCallback = require('./app/routes/book-callback');
+var serviceDisplay = require('./app/routes/service-display');
 
 var userJourneys = require('./app/routes/user-journeys');
 
@@ -104,6 +105,7 @@ app.use('/questions', questions);
 app.use('/forced-callback', forcedCallback);
 app.use('/callback-offered', callbackOffered);
 app.use('/book-callback', bookCallback);
+app.use('/service-display', serviceDisplay);
 
 app.use('/user-journeys', userJourneys);
 
