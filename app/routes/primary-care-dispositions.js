7ddb95f55e419d12fe55265d218ad192ee6a9a01
demotive <demotive@gmail.com>
2019-12-05 15:42:41 +0000

    A virtually link and logo

--- app/routes/primary-care-dispositions.js
+++ app/routes/primary-care-dispositions.js
@@ -4,6 +4,12 @@ var router = express.Router()
 
 module.exports = router
 
+router.get('/gpoc-init', function(req, res) {
+  // build out a namespaced session obj
+  req.session.gpoc = req.query;
+  res.redirect('/user-journeys/scenario-003-primary-care');
+});
+
 router.post('/handover-launcher', function(req, res) {
   if (req.body['appt-type'] === 'online-consultation') {
     res.redirect('/primary-care-dispositions/iteration-4-gpoc/handover-launcher');
